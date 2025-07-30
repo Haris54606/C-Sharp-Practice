fa0a657762471ecc32f27a16e758e1af1323c985
Haris54606 <72005605+Haris54606@users.noreply.github.com>
2025-07-30 17:15:30 +0500

    Add files via upload

---
+++ Abstraction/Abstract_propertise/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Abstract_propertise
+{
+    public abstract class person
+    {
+        public abstract int Id { get; set; }
+        public abstract string Name { get; set; }
+    }
+    
+    public class Student : person
+    {
+        int student_id;
+        string Student_Name;
+        public override int Id {
+            set
+            {
+                this.student_id = value;
+            }
+            get
+            {
+                return this.student_id;
+            }
+             }
+        public override string Name {
+            set { 
+                this.Student_Name = value;
+            }
+            get { 
+                return this.Student_Name; }
+             }
+
+    }
+    public class Teacher
+    {
+
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Id = 12;
+            s.Name = "Haris";
+            Console.WriteLine(s.Id);
+            Console.WriteLine(s.Name);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class employ
+    {
+        public int id;
+        public string name;
+        public double grosspay;
+        double text_deduction = 0.1;
+        double netsalary;
+
+
+        //constructor
+        public employ(int empid, string empName, double empGrosspay)
+        {
+            this.id = empid;
+            this.name = empName;
+            this.grosspay = empGrosspay;
+
+        }
+        // agr koi acces modifier nh lagao to by default private hoga 
+        void calculate_Salary()
+        {
+            if (grosspay >= 30000)
+            {
+                netsalary = grosspay - (text_deduction * grosspay);
+                Console.WriteLine("Your salary is : {0}",netsalary);
+            }
+            else
+            {
+                Console.WriteLine("your salary is : {0}",grosspay);
+            }
+        }
+
+        public void show_employe_salary()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine("employe Name is : {0}",this.name);
+            this.calculate_Salary();
+        }
+
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                employ e = new employ(17, "haris", 10000);
+                e.show_employe_salary();
+
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}

c40f70334dba1a445033ed7f341047a7ade1d769
Haris54606 <72005605+Haris54606@users.noreply.github.com>
2025-07-30 17:33:26 +0500

    Add files via upload

---
+++ AccesModifier/AccesModifier/Program1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AccesModifier
+{
+   public class class1
+    {
+        public void show1()
+        {
+            Console.WriteLine("this is show1 method");
+        }
+        // ---- ---- show1 method calling in show2 method --- -->
+        public void show2()
+        {
+            class1 c = new class1();
+            c.show1();
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            //class1 c = new class1();
+            //c.show1();
+            class1 c = new class1();
+            c.show2();
+            Console.WriteLine("AccesModifier 1");
+        }
+    }
+}
---
+++ AccesModifier/AccesModifier2/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AccesModifier;
+
+namespace AccesModifier2
+{
+    class abc : class1
+    {
+       public void showAbc()
+        {
+            Console.WriteLine("AccesModifier2 chilled class ");
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            // ----- calling class1 from acces modifier 1 - -------> 
+            //class1 c = new class1();
+            //c.show1();
+
+
+            // ----- calling class1 from acces modifier 2 (child class) - ------->
+            abc q = new abc();
+            q.show1();
+
+            // ---- calling chiled class method ---------  ---->
+            q.showAbc();
+            Console.WriteLine("AccesModifier2");
+        }
+    }
+}
